d3a1a7e43307230bd11fe2bb99797302e9190a50
NhutPhat04 <phatluong816@gmail.com>
2024-12-08 22:56:20 +0700

    Add project files.

---
+++ WindowsFormsApp4/Form2.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMSNV = new System.Windows.Forms.TextBox();
+            this.txtTenNV = new System.Windows.Forms.TextBox();
+            this.txtLuongCB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_DongY = new System.Windows.Forms.Button();
+            this.btnBoQua = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMSNV
+            // 
+            this.txtMSNV.Location = new System.Drawing.Point(257, 47);
+            this.txtMSNV.Name = "txtMSNV";
+            this.txtMSNV.Size = new System.Drawing.Size(152, 20);
+            this.txtMSNV.TabIndex = 3;
+            // 
+            // txtTenNV
+            // 
+            this.txtTenNV.Location = new System.Drawing.Point(257, 100);
+            this.txtTenNV.Name = "txtTenNV";
+            this.txtTenNV.Size = new System.Drawing.Size(152, 20);
+            this.txtTenNV.TabIndex = 4;
+            // 
+            // txtLuongCB
+            // 
+            this.txtLuongCB.Location = new System.Drawing.Point(257, 155);
+            this.txtLuongCB.Name = "txtLuongCB";
+            this.txtLuongCB.Size = new System.Drawing.Size(152, 20);
+            this.txtLuongCB.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mã NV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tên NV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Lương CB";
+            // 
+            // btn_DongY
+            // 
+            this.btn_DongY.Location = new System.Drawing.Point(113, 236);
+            this.btn_DongY.Name = "btn_DongY";
+            this.btn_DongY.Size = new System.Drawing.Size(75, 23);
+            this.btn_DongY.TabIndex = 9;
+            this.btn_DongY.Text = "Đồng ý";
+            this.btn_DongY.UseVisualStyleBackColor = true;
+            this.btn_DongY.Click += new System.EventHandler(this.btn_DongY_Click);
+            // 
+            // btnBoQua
+            // 
+            this.btnBoQua.Location = new System.Drawing.Point(288, 236);
+            this.btnBoQua.Name = "btnBoQua";
+            this.btnBoQua.Size = new System.Drawing.Size(70, 23);
+            this.btnBoQua.TabIndex = 10;
+            this.btnBoQua.Text = "Bỏ Qua";
+            this.btnBoQua.UseVisualStyleBackColor = true;
+            this.btnBoQua.Click += new System.EventHandler(this.btnBoQua_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBoQua);
+            this.Controls.Add(this.btn_DongY);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLuongCB);
+            this.Controls.Add(this.txtTenNV);
+            this.Controls.Add(this.txtMSNV);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMSNV;
+        private System.Windows.Forms.TextBox txtTenNV;
+        private System.Windows.Forms.TextBox txtLuongCB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_DongY;
+        private System.Windows.Forms.Button btnBoQua;
+    }
+}
---
+++ WindowsFormsApp4/Form2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form2 : Form
+    {
+        public string MSNV { get; set; }
+        public string TenNV { get; set; }
+        public string LuongCB { get; set; }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_DongY_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra giá trị không được để trống
+            if (string.IsNullOrWhiteSpace(txtMSNV.Text) ||
+                string.IsNullOrWhiteSpace(txtTenNV.Text) ||
+                string.IsNullOrWhiteSpace(txtLuongCB.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Kiểm tra Lương cơ bản là số
+            if (!decimal.TryParse(txtLuongCB.Text, out _))
+            {
+                MessageBox.Show("Lương cơ bản phải là một số hợp lệ.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Lưu dữ liệu
+            MSNV = txtMSNV.Text.Trim();
+            TenNV = txtTenNV.Text.Trim();
+            LuongCB = txtLuongCB.Text.Trim();
+
+            // Đặt kết quả và đóng form
+            this.DialogResult = DialogResult.OK; // Trả về OK
+            this.Close();
+        }
+
+        private void btnBoQua_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel; // Trả về Cancel
+            this.Close();
+        }
+
+        // Khi form được tải
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // Gán dữ liệu cũ vào các ô nhập liệu (nếu có)
+            txtMSNV.Text = MSNV;
+            txtTenNV.Text = TenNV;
+            txtLuongCB.Text = LuongCB;
+        }
+    }
+}
